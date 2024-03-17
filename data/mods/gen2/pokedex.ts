ca124bcf0cb321d4e6abaff724ac1d52a688d203
anastarawneh <anastarawneh2001@icloud.com>
2024-03-17 16:43:46 +0300

    Update pokedex.ts

--- data/mods/gen2/pokedex.ts
+++ data/mods/gen2/pokedex.ts
@@ -1,4 +1,8 @@
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	arbok: {
+		inherit: true,
+		baseStats: {hp: 60, atk: 85, def: 69, spa: 65, spd: 79, spe: 80},
+	},
 	unown: {
 		inherit: true,
 		cosmeticFormes: ["Unown-B", "Unown-C", "Unown-D", "Unown-E", "Unown-F", "Unown-G", "Unown-H", "Unown-I", "Unown-J", "Unown-K", "Unown-L", "Unown-M", "Unown-N", "Unown-O", "Unown-P", "Unown-Q", "Unown-R", "Unown-S", "Unown-T", "Unown-U", "Unown-V", "Unown-W", "Unown-X", "Unown-Y", "Unown-Z"],
