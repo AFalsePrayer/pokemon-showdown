12632e62651b70b676a781f92aad31dd8f0d0d2a
anastarawneh <anastarawneh2001@icloud.com>
2024-03-19 23:00:20 +0300

    Update moves.ts

--- data/mods/gen3emeraldkaizo/moves.ts
+++ data/mods/gen3emeraldkaizo/moves.ts
@@ -2,5 +2,38 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 	cut: {
 		inherit: true,
 		accuracy: 100,
+	},
+	whirlwind: {
+		inherit: true,
+		pp: 5,
+	},
+	bind: {
+		inherit: true,
+		basePower: 100,
+	},
+	rollingKick: {
+		inherit: true,
+		basePower: 100,
+		pp: 24
+	},
+	sandattack: {
+		inherit: true,
+		pp: 5
+	},
+	drillrun: {
+		inherit: true,
+		num: 529,
+		accuracy: 95,
+		basePower: 80,
+		category: "Physical",
+		name: "Drill Run",
+		pp: 10,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1, metronome: 1},
+		critRatio: 2,
+		secondary: null,
+		target: "normal",
+		type: "Ground",
+		contestType: "Tough",
 	}
 };
