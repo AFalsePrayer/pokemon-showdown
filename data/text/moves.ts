0288c1ffe1a6b00a4987f7d75417eb8d478f5e11
anastarawneh <anastarawneh2001@icloud.com>
2024-03-20 15:29:31 +0300

    Update moves.ts

--- data/text/moves.ts
+++ data/text/moves.ts
@@ -6843,11 +6843,11 @@ export const MovesText: {[k: string]: MoveText} = {
 	},
 	superpower: {
 		name: "Superpower",
-		desc: "If the target lost HP, the user takes recoil damage equal to 1/3 the HP lost by the target, rounded down, but not less than 1 HP.",
-		shortDesc: "Has 1/3 recoil.",
+		desc: "Lowers the user's Attack and Defense by 1 stage.",
+		shortDesc: "Lowers the user's Attack and Defense by 1.",
 		gen3: {
-			desc: "Lowers the user's Attack and Defense by 1 stage.",
-			shortDesc: "Lowers the user's Attack and Defense by 1.",
+			desc: "If the target lost HP, the user takes recoil damage equal to 1/3 the HP lost by the target, rounded down, but not less than 1 HP.",
+			shortDesc: "Has 1/3 recoil.",
 		},
 	},
 	supersonic: {
